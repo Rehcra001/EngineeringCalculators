13b061cf20e52170a348f8c9287fe878b4197afd
Rehcra001 <137676462+Rehcra001@users.noreply.github.com>
2024-12-26 10:08:09 +0200

    Added search and filter features for materials

--- EngineeringCalculators/EngineeringCalculators.Web/Pages/Material.razor.cs
+++ EngineeringCalculators/EngineeringCalculators.Web/Pages/Material.razor.cs
@@ -8,6 +8,10 @@ namespace EngineeringCalculators.Web.Pages
     {
         private MaterialModel _material = new();
         private List<MaterialModel> _materials = [];
+        private List<string> _filterByCategory = [];
+        private List<MaterialModel> _filteredMaterial = [];
+        private string _searchText = "";
+        private string _selectedCategory = "All";
         private bool _canSave = true;
         private bool _disabled = false;
 
@@ -24,6 +28,8 @@ namespace EngineeringCalculators.Web.Pages
         private async Task HandleLoadingMaterialAsync()
         {
             _materials = await MaterialService.GetAllAsync();
+            _filteredMaterial = _materials;
+            GetCategories();
         }
 
         private void HandleNewMaterial()
@@ -35,6 +41,11 @@ namespace EngineeringCalculators.Web.Pages
 
         private async Task HandleSaveAsync(MaterialModel material)
         {
+            if (String.IsNullOrWhiteSpace(material.Category))
+            {
+                material.Category = "--";
+            }
+
             if (material.Id == 0) // New Material
             {
                 if (_materials.Count == 0)
@@ -46,10 +57,10 @@ namespace EngineeringCalculators.Web.Pages
                     int max = _materials.Max(x => x.Id);
                     material.Id = max + 1;
                 }
-                _materials.Add(material);             
+                _materials.Add(material);
             }
 
-           
+
 
             if (MaterialService.FileHandle is null)
             {
@@ -62,6 +73,10 @@ namespace EngineeringCalculators.Web.Pages
 
             _canSave = false;
             _disabled = true;
+
+            HandleFilterByCategory(_selectedCategory);
+
+            GetCategories();
         }
 
         private void HandleEdit()
@@ -69,5 +84,43 @@ namespace EngineeringCalculators.Web.Pages
             _canSave = true;
             _disabled = false;
         }
+
+        private void HandleFilterByCategory(string category)
+        {
+            _selectedCategory = category;
+
+
+            if (_selectedCategory.Equals("All"))
+            {
+                _filteredMaterial = _materials;
+            }
+            else
+            {
+                _filteredMaterial = _materials.Where(x => x.Category.Equals(_selectedCategory)).ToList();
+            }
+
+            if (String.IsNullOrWhiteSpace(_searchText) == false)
+            {
+                _filteredMaterial = _filteredMaterial.Where(x => x.Name.Contains(_searchText)).ToList();
+            }
+        }
+
+        private void HandleSearchText()
+        {
+            HandleFilterByCategory(_selectedCategory);
+        }
+
+        private void HandleClearSearch()
+        {
+            _searchText = "";
+            HandleFilterByCategory(_selectedCategory);
+        }
+
+        private void GetCategories()
+        {
+            _filterByCategory = _materials.Select(x => x.Category).Distinct().ToList();
+        }
     }
+
+
 }
