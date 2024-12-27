b8426a4017778d2a1d0d6b56354eb0f4d9a74024
Rehcra001 <137676462+Rehcra001@users.noreply.github.com>
2024-12-27 16:36:46 +0200

    Sheet Metal WIP Save

---
+++ EngineeringCalculators/EngineeringCalculators.Web/Models/SheetMetal/BlankAndPierceModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EngineeringCalculators.Web.Models.SheetMetal
+{
+    public class BlankAndPierceModel
+    {
+        public int Id { get; set; }
+        public string ProjectName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public double Perimeter { get; set; }
+        public double ForceRequired { get; set; }
+        public double StrippingForce { get; set; }
+        public double Clearance { get; set; }
+    }
+}
---
+++ EngineeringCalculators/EngineeringCalculators.Web/Models/SheetMetal/SheetMetalProjectModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EngineeringCalculators.Web.Models.SheetMetal
+{
+    public class SheetMetalProjectModel
+    {
+        public string ProjectName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public double Thickness { get; set; }
+        public  MaterialModel? Material { get; set; }
+    }
+}
